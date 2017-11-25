ae9c7d961da07fa6e7b22913bad4ae8782e14504
Rolando <rolando_ar95@hotmail.com>
2017-11-25 16:35:31 -0500

    vista: jugador y caracteristicas

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -6,29 +6,71 @@
 
 
 @section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-
-				<!-- Default box -->
-				<div class="box">
-					<div class="box-header with-border">
-						<h3 class="box-title">Home</h3>
-
-						<div class="box-tools pull-right">
-							<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-								<i class="fa fa-minus"></i></button>
-							<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-								<i class="fa fa-times"></i></button>
-						</div>
-					</div>
-					<div class="box-body">
-						{{ trans('adminlte_lang::message.logged') }}. Start creating your amazing application!
-					</div>
-					<!-- /.box-body -->
+	<h1>Nombre del Equipo</h1>
+	<div class="contenedor">
+		<div class="columnas1">
+			<div class="jugador">
+				<div class="imagen">
+					<img src="#" alt="jugador X">
 				</div>
-				<!-- /.box -->
+				<div class="grafico">
+					<img src="#" alt="jugador X">
+				</div>
+			</div>
 
+			<div class="jugadores">
+				<h2>Lista de jugadores</h2>
+				<div class="lista">
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>
+					<p class="nombres">XXXXXXXXXX</p>					
+				</div>				
+			</div>
+		</div>
+
+		<div class="columnas2">
+			<div class="caracteristicas propias">
+				<h2>Datos del jugador</h2>
+				<p class="etiquetas">Nombre del jugador</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Apellidos del jugador</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Fecha de nacimiento</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Lugar de nacimiento</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Grupo sanguíneo</p>
+				<p class="datos">xxxx</p>
+			</div>
+			<div class="caracteristicas detalle">
+				<h2>Detalles del jugador</h2>
+				<p class="etiquetas">Pases completos</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Pases errados</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Total de pases</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Asistencias</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Porcentaje de pases</p>
+				<p class="datos">xxxx</p>
+				<p class="etiquetas">Minutos jugados</p>
+				<p class="datos">xxxx</p>						
 			</div>
 		</div>
 	</div>
