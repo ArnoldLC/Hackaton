e10ce519cad92f7555cdc3af531434b3f124bcdd
Renato <renato.msd.1407@gmail.com>
2017-11-25 19:04:03 -0500

    mig v8

--- database/migrations/2017_11_25_200102_create_equipo_table.php
+++ database/migrations/2017_11_25_200102_create_equipo_table.php
@@ -21,7 +21,7 @@ class CreateEquipoTable extends Migration
             $table->date('fecha_fundacion');
             $table->string('ciudad');
             $table->string('pais');  
-            $table->integer('user_id')->unsigned(); ;  
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');          
             $table->rememberToken();
             $table->timestamps();
